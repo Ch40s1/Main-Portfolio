fe6b8a06307355a6ea11b9f241e97df9ca42a417
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-12-26 22:45:51 -0600

    intalled new packages to try

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import avatar from '../img/avatar.png';
 
+
 function Home() {
   return (
     <>
-      <div className="text-mainText font-base">
+      <div className="text-mainText font-base ml-10">
         <h2 className='text-base'>Full-Stack Software Developer</h2>
         <p>
-        Resolving design problems,
-        building smart user interfaces and useful interactions,
+        Resolving design problems,<br/>
+        building smart user interfaces and useful interactions,<br/>
         developing rich web applications and seamless web experiences.
         </p>
       </div>
-      <div>
+      <div className='ml-20'>
         <img
           src={avatar}
           width={800}
