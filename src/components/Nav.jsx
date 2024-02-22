6acd428605faefc4cc75200505d14fd57e514e1a
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-02-21 23:34:34 -0600

    moving linked to navbar"

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,5 @@
+import githubSvg from '../img/icons/github.svg';
+
 function NavBar() {
 
   const styles = {
@@ -28,6 +30,7 @@ function NavBar() {
       <div className='flex p-5 justify-between items-center'>
         <div>
           {/* there will a box that will animate as if tho typing */}
+          {/* ima switch the typing to my regular name  */}
           <h1 className="lg:text-base">
             <a href='https://github.com/Ch40s1' className="text-offText underline">ch40s1</a>
             @Daniels-Portfolio ~ %
@@ -35,6 +38,12 @@ function NavBar() {
               |
             </span>
           </h1>
+        </div>
+        <div className="flex gap-4">
+          <a>Projects</a>
+          <a>github</a>
+          <a>linked in</a>
+          <img src={githubSvg} width={30} />
         </div>
         <nav className="border-solid border-white border-2 px-4 py-2 rounded-lg">
           <a href='https://www.linkedin.com/in/daniel-zarate-66816113b/' target="blank">Contact</a>
