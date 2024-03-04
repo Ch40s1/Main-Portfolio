87988d63e79fd4cbbcbf47ac9c2a22279c89af56
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-03-04 16:40:40 -0600

    finished new navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,13 +7,14 @@ function NavBar() {
 
   return (
     <>
-      <div className='md:flex p-5 justify-between items-center bg-slate-300'>
-        <div className='flex w-full md:w-1/2 justify-between bg-slate-500'>
-          <h1 className='text-[2rem]'>Daniels Zarate</h1>
+      <div className='md:flex p-5 justify-between items-center'>
+        <div className='flex w-full md:w-1/2 justify-between pl-6'>
+          <h1 className='text-[2rem]'>Daniels <span className='text-gray-400'>Zarate</span></h1>
           <img src={bars} width={40} onClick={()=>setOpen(!open)} className='md:hidden'></img>
         </div>
-        <nav className={`justify-around flex flex-col items-center md:flex-row md:items-center md:pb-0 pb-12 absolute md:static bg-slate-600 md:z-auto left-0 w-full md:h-auto md:w-1/2 md:pl-0 pl-9 ${open ? 'top-20 h-[300px]': '-top-[420px]'}`}>
-          <a>Projects</a>
+        <nav className={`justify-around flex flex-col items-center md:flex-row md:items-center bg-mainBlack md:pb-0 pb-12 absolute md:static md:z-auto left-0 w-full md:h-auto md:w-1/2 md:pl-0 pl-9 ${open ? 'top-20 h-[300px]': '-top-[420px]'}`}>
+          <a href='#Projects' className='text-[#3CF670]'>* Projects *</a>
+          <a href='#About'>About Me</a>
           <a
             href="https://www.linkedin.com/in/daniel-zarate-66816113b/"
             target="blank"
@@ -23,7 +24,7 @@ function NavBar() {
               alt="LinkedIn Badge"
             />
           </a>
-          <a href='https://github.com/Ch40s1' className="text-offText underline" target='blank'><img src={githubSvg} width={38} /></a>
+          <a href='https://github.com/Ch40s1' target='blank'><img src={githubSvg} width={38} /></a>
           <a href='https://www.linkedin.com/in/daniel-zarate-66816113b/' target="blank">Contact</a>
         </nav>
       </div>
